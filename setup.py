bd9a5a11f37850d506f202fa30c76eff68bd4cd1
RakeshHG <h077rakesh@gmail.com>
2025-04-23 22:39:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ setup(
         'matplotlib=3.1.0',
         'seaborn=0.10.0'
     ],
-    author='Your Name',
-    author_email='your@email.com',
+    author='Rakesh H G',
+    author_email='h077rakesh@gmail.com',
     description='A Python package for processing workload CSVs using a Variational Autoencoder (VAE)',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='textmarkdown',
-    url='httpsgithub.comyourusernameworkload_vae',
+    url='https://github.com/RakeshHG/workload_vae',
     classifiers=[
         'Programming Language  Python  3',
         'License  OSI Approved  MIT License',
